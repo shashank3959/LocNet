d7865f3d0015d4bf3a2c97461294826dd4b014ca
Deepan Das <deepandas11@gmail.com>
2019-05-18 19:13:27 -0400

    Shuffling script used for Flickr30K dataset
    Previously shuffling was on main script. This is aimed to reduce runtime
    for main script and ease of use. Now all files are segregated. The fold
    used here is that on the github page for flickr30k dataset.
    
    Signed-off-by: Deepan Das <deepandas11@gmail.com>

---
+++ data/flickr_30kentities/flickr_data_shuffler.py
@@ -0,0 +1,42 @@
+import os
+
+
+def move_to_dest(token, f_type, fold):
+    mod_token = token + file_types[f_type][0]
+    f_name = os.path.join(file_types[f_type][1], mod_token)
+    new_fname = os.path.join(file_types[f_type][1], fold, mod_token)
+    os.rename(f_name, new_fname)
+
+
+if __name__ == '__main__':
+
+    fold_files = {"train": "train.txt",
+                  "val": "val.txt",
+                  "test": "test.txt"}
+
+    img_folder = 'flickr30k-images'
+    sent_folder = os.path.join('annotations_flickr', 'Sentences')
+    ann_folder = os.path.join('annotations_flickr', 'Annotations')
+
+    file_types = {"img": ['.jpg', img_folder],
+                  "sent": ['.txt', sent_folder],
+                  "ann": ['.xml', ann_folder]}
+
+    for fold in fold_files:
+
+        os.mkdir(os.path.join(img_folder, fold))
+        os.mkdir(os.path.join(sent_folder, fold))
+        os.mkdir(os.path.join(ann_folder, fold))
+
+        print("Now doing fold: ", fold)
+        with open(fold_files[fold], encoding='utf-8', mode='r') as f:
+            count = 0
+            for file_name in f:
+                if file_name[-1] == '\n':
+                    move_to_dest(file_name[:-1], "img", fold)
+                    move_to_dest(file_name[:-1], "sent", fold)
+                    move_to_dest(file_name[:-1], "ann", fold)
+
+                    count += 1
+
+            print("Done with fold : {fold},\n number of files: {count}".format(fold=fold, count=count))
