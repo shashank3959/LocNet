b6ba8da70c835af8d725da5ef0b1e9e7a1f297ae
Deepan Das <deepandas11@gmail.com>
2019-05-23 21:58:37 -0500

    Adding default parse mode functionality to dataloader
    Signed-off-by: Deepan Das <deepandas11@gmail.com>

--- dataloader/flickr_loader.py
+++ dataloader/flickr_loader.py
@@ -38,7 +38,7 @@ class FlickrDataset(data.Dataset):
     def __init__(self, transform, mode, batch_size, sentences_root, annotations_root,
                  image_root, vocab_glove_file, start_word='<start>',
                  end_word='<end>', unk_word='<unk>', pad_caption=True,
-                 pad_limit=30, parse_mode='phrase', fold='train'):
+                 pad_limit=20, parse_mode='phrase', fold='train'):
         self.transform = transform
         self.mode = mode
         self.batch_size = batch_size
@@ -77,8 +77,8 @@ class FlickrDataset(data.Dataset):
             self.caption_lengths = [len(caption) for caption in all_tokenized_captions]
 
     def __getitem__(self, index):
-        # Obtain image and caption if in training and validation mode
-        if self.mode in ['train', 'val'] and self.parse_mode == 'phrase':
+        # Obtain image and caption if in 'phrase' parse mode
+        if self.mode in ['train', 'val', 'test'] and self.parse_mode == 'phrase':
             ann_id = self.ids[index]
             image_file = self.sentences[ann_id]['image_file']
             image = Image.open(os.path.join(self.image_folder, image_file)).convert("RGB")
@@ -91,12 +91,36 @@ class FlickrDataset(data.Dataset):
             caption.append(self.end_word)
 
             if self.pad_caption:
-                caption.append([self.end_word] * (self.pad_limit - len(caption_tokens)))
+                caption.extend([self.end_word] * (self.pad_limit - len(caption_tokens)))
 
             caption_gloves = torch.Tensor([self.token_glove_generator(item) \
                                            for item in caption])
 
             return image, caption_gloves, caption
+
+        elif self.mode in ['train', 'val', 'test'] and self.parse_mode == 'default':
+            ann_id = self.ids[index]
+            image_file = self.sentences[ann_id]['image_file']
+            image = Image.open(os.path.join(self.image_folder, image_file)).convert("RGB")
+            image = self.transform(image)
+
+            caption_tokens = self.sentences[ann_id]['tok_sent']
+            caption = list()
+            caption.append(self.start_word)
+            caption.extend(caption_tokens)
+            caption.append(self.end_word)
+
+            if self.pad_caption:
+                caption.extend([self.end_word] * (self.pad_limit - len(caption_tokens)))
+
+            caption_gloves = torch.Tensor([self.token_glove_generator(item) \
+                                           for item in caption])
+
+            return image, caption_gloves, caption
+
+
+
+
 
     def get_glove(self, word):
         if word not in self.vocab_glove.keys():
@@ -104,7 +128,10 @@ class FlickrDataset(data.Dataset):
         return self.vocab_glove[word]
 
     def token_glove_generator(self, token):
-        if len(token) == 1:
+        if type(token) is not list:
+            return self.get_glove(token)
+
+        elif len(token) == 1:
             return self.get_glove(token[0])
 
         else:
@@ -122,6 +149,9 @@ class FlickrDataset(data.Dataset):
             sel_length = np.random.choice(self.caption_lengths)
             all_indices = np.where([self.caption_lengths[i] == \
                                     sel_length for i in np.arange(len(self.caption_lengths))])[0]
+
+        indices = list(np.random.choice(all_indices, size=self.batch_size))
+        return indices
 
     def __len__(self):
         return len(self.ids)
