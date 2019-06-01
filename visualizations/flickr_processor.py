d20f2418dc9c347a49a4a252010b2ce3eaea963c
deepandas11 <deepandas11@gmail.com>
2019-05-31 20:49:21 -0500

    Added functionality to show localization maps
    Can read flickr test data in whatever parse mode and show localization
    maps for the entities provided in that caption. The entity phrases are
    provided in the flickr dataset.
    
    If parse_mode is default, colocmaps are found for each word. Then,
    colocmaps for the words in a given entity phrase is averaged and
    visualized.
    
    If parse_mode is phrase, colocmaps for each phrase is evaluated and
    
    Signed-off-by: deepandas11 <deepandas11@gmail.com>

--- visualizations/flickr_processor.py
+++ visualizations/flickr_processor.py
@@ -17,7 +17,7 @@ transform = transforms.Compose([transforms.Resize((224, 224)),
                                 transforms.Normalize((0.485, 0.456, 0.406),(0.229, 0.224, 0.225))])
 
 
-def load_data(batch_size, parse_mode='phrase'):
+def load_data(batch_size, parse_mode):
     """
     Loads data from test fold of flickr dataset using flickr_loader
     :param parse_mode: decides whether the captions should be parsed 
@@ -25,45 +25,37 @@ def load_data(batch_size, parse_mode='phrase'):
     """
     flickr_loader = get_loader_flickr(transform=transform, batch_size=batch_size,
                                       mode='test', parse_mode=parse_mode)
-    
+
     for batch in flickr_loader:
         (image, caption_glove, caption, ids) = batch
 
     return image, caption_glove, ids
 
 
-def list2string(query_list):
+def gen_data_element(index, batch_size=1, parse_mode='phrase',
+                     model_path='..saved_models/checkpoint.pth.tar'):
     """
-    Convert a list of strings to a space separated string
+    Generates a dictionary of data elements from flickr dataset
+    :param index: choose data from a batch using this index
+    :param batch_size: choose to select a specific batch size
+    :param parse_mode: choose whether to parse flickr captions based on entities
+    :param model_path: path to trained model, if exists.
+    :return element: element is a dictionary containing the following elements
+    :return element['image']['col']: 3 channel color image
+    :return element['image']['bw']: single channel bw image
+    :return element['caption']: tokenized and clipped caption depending on parse_mode
+    :return element['coloc_map']: resized and clipped coloc_map masks for the given data point
     """
-    assert type(query_list) is list, "Lists accepted only"
-    separator = ' '
-    final_string = separator.join(query_list)
-    return final_string
+    assert index < batch_size, "Index out of range"
 
+    image_model, caption_model = get_models(model_path)
+    image_tensor, caption_glove, ids = load_data(batch, parse_mode=parse_mode)
+    coloc_maps = gen_coloc_maps(image_model, caption_model,
+                                image_tensor, caption_glove)
+    raw_element = fetch_data(index, coloc_maps, image_tensor, ids)
 
+    element = flickr_element_processor(raw_element, parse_mode, data)
 
-def clip_list(query_list, ann_id, data, parse_mode):
-    if parse_mode == 'phrase':
-        actual_length = len(data[ann_id]['parsed_caption'])
-        caption = data[ann_id]['parsed_caption']
-    else:
-        actual_length = len(data[ann_id]['tok_sent'])
-        caption = data[ann_id]['tok_sent']
-    start = actual_length
-    del query_list[(start + 1):]
-    del query_list[0]
-    return query_list, caption
+    return element
 
-
-def master_processor(element, ):
-    co_loc_map, ann_id = element['co_loc_map'], elemenet['caption']
-
-    color_image, bw_image = element['image']
-    mask_list = list()
-    for mmap in matchmap:
-        mask = cv2.resize(mmap, dsize=(224,224))
-        mask_list.append(mask)
-    mask_list = np.stack(mask_list, axis = 0)
-    return color_image, bw_image, mask_list, caption
 
