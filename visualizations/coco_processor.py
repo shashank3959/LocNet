b2769f202d5fed32e00fbc8ca6de9122177824d5
Deepan Das <deepandas11@gmail.com>
2019-06-01 09:15:01 -0500

    Class module from coco dataset visualization.

--- visualizations/coco_processor.py
+++ visualizations/coco_processor.py
@@ -1,50 +1,71 @@
+from .visualize_utils import *
+
+from torchvision import transforms
+import json
+import matplotlib.pyplot as plt
 import numpy as np
-import cv2
+from statistics import mean
+from tqdm import tqdm
+
+from steps.utils import *
+
+transform = transforms.Compose([transforms.Resize((224, 224)),
+                                transforms.ToTensor(),
+                                transforms.Normalize((0.485, 0.456, 0.406),(0.229, 0.224, 0.225))])
 
 
-def rgb2gray(rgb):
-    # Convert color numpy image to grayscale
-    return np.dot(rgb[..., :3], [0.2989, 0.5870, 0.1140])
+
+class COCOViz():
+
+    def __init__(self, batch_size, model_path='saved_models/checkpoint.pth.tar',
+                 mode='val', transform=transform):
+
+        self.batch_size = batch_size
+        self.model_path = model_path
+        self.mode = mode
+        self.transform = transform
+
+        self.image_model, self.caption_model = get_models(self.model_path)
+
+        self.image_tensor, self.caption_glove, self.caption = coco_load_data(self.batch_size,
+                                                                             self.transform,
+                                                                             self.mode)
+
+        self.coloc_maps = gen_matchmap(self.image_model, self.caption_model,
+                                           self.image_tensor, self.caption_glove)
+
+    def __getitem__(self, index):
+
+        element = fetch_data(index, self.coloc_maps, self.image_tensor, self.caption)
+        self.element = coco_element_processor(element)
+
+        return self.element
+
+    def __call__(self, save_flag=False, seg_flag=False, thresh=0.5):
+        element = self.element
+        color_img = 
+        
 
 
-def tensor2img(tensor_image):
-    # Convert each tensor image to numpy image
-    img = tensor_image.permute(1, 2, 0)
-    color_img = img.numpy()
-    bw_img = rgb2gray(color_img)
 
-    return color_img, bw_img
 
-def caption_list_gen(caption):
-    """
-    Dataloader parses caption as a list of tuples
-    Each tuple holds positional words
-    This makes it a list of lists of each sentence's tokenized words.
-    """
-    caption_list = []
-    for i in range(len(caption[0])):  # Number of sentences
-        new_caption = []  # For each sentence a new list
-        for j in range(len(caption)):  # Number of tokens
-            new_caption.append(caption[j][i])  # Go to jth tuple, ith list
-        caption_list.append(new_caption)  # Generate the respective caption
-    return caption_list  # Return tokenized caption
 
-def clip_list(query_list, caption):
-    start = caption.index('<end>')
-    del [caption[start:]]
-    del [query_list[start:]]
-    del [caption[0]]
-    del [query_list[0]]
 
-    return query_list, caption
 
-def master_processor(element):
-    matchmap, caption = clip_list(element['matchmap'],
-                                  element['caption'])
-    color_image, bw_image = element['image']
-    mask_list = list()
-    for mmap in matchmap:
-        mask = cv2.resize(mmap, dsize=(224,224))
-        mask_list.append(mask)
-    mask_list = np.stack(mask_list, axis = 0)
-    return color_image, bw_image, mask_list, caption
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
