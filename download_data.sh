68000ac10a1e9115691874ffaec3c93103256abd
deepandas11 <deepandas11@gmail.com>
2019-06-01 17:21:20 -0500

    Shell script to download necessary data in required structure
    Signed-off-by: deepandas11 <deepandas11@gmail.com>

--- download_data.sh
+++ download_data.sh
@@ -24,3 +24,28 @@ rm flickr30k.tar.gz
 mkdir annotations_flickr
 mv Annotations annotations_flickr/
 mv Sentences annotations_flickr/
+
+
+cd ..
+mkdir mscoco
+cd mscoco
+
+wget http://images.cocodataset.org/zips/train2014.zip
+wget http://images.cocodataset.org/annotations/annotations_trainval2014.zip
+wget http://images.cocodataset.org/zips/val2014.zip
+
+mkdir images
+mv train2014.zip images/
+mv val2014.zip images/
+
+
+unzip images/train2014.zip
+rm images/train2014.zip 
+
+unzip images/val2014.zip
+rm images/val2014.zip
+
+unzip annotations_trainval2014.zip
+rm annotations_trainval2014.zip
+
+cd ../..
