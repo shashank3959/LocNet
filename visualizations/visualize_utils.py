afee0bd230aa332b0f55da4f3c77a6274e9c3bc2
deepandas11 <deepandas11@gmail.com>
2019-05-31 20:50:30 -0500

    Added functionality to show localization maps
    Can read flickr test data in whatever parse mode and show localization
    maps for the entities provided in that caption. The entity phrases are
    provided in the flickr dataset.
    
    If parse_mode is default, colocmaps are found for each word. Then,
    colocmaps for the words in a given entity phrase is averaged and
    visualized.
    
    If parse_mode is phrase, colocmaps for each phrase is evaluated and
    
    Signed-off-by: deepandas11 <deepandas11@gmail.com>

--- visualizations/visualize_utils.py
+++ visualizations/visualize_utils.py
@@ -25,7 +25,7 @@ from steps.utils import matchmap_generate
 from models import VGG19, LSTMBranch
 
 
-def get_models(model_path='..saved_models/checkpoint.pth.tar'):
+def get_models(model_path):
     '''
     Load pre-trained model
     :return: pretrained image and caption model
@@ -76,7 +76,8 @@ def fetch_data(index, coloc_maps, image_tensor, captions):
     element = dict()
     element['coloc_map'] = coloc_maps[index]
     element['image'] = image_tensor[index]
-    element['caption'] = captions[index]  # In case of flickr, AnnID
+    element['caption'] = captions[index] # In case of flickr, AnnID
+    element['name'] = str(index)
 
     return element
 
@@ -117,6 +118,34 @@ def coloc_map_processor(coloc_map):
     return mask_list
 
 
+def caption_parser(tok_caption, coloc_maps, index_list):
+    """
+    When parse_mode for flickr is not phrase, tokenized caption is passed into data
+    Use this tokenized data to generate a parsed caption and average of the 
+    associated positional colocalization maps
+    :param tok_caption: Tokenized caption without end and start words
+    :param coloc_maps: clipped list of colocalization maps. 
+    :param index_list: list of positional indices to convert tok_caption to tok_phrase
+    :return parsed_caption: list of phrases. each phrase is a list
+    :return parsed_coloc_maps: list of averaged colocalization maps. 
+    """
+    parsed_caption = list()
+    parsed_coloc_maps = list()
+
+    for position in index_list:
+        new_word_token = list()
+        new_coloc_map = list()
+        for index in position:
+            new_word_token.append(tok_caption[index])
+            new_coloc_map.append(coloc_maps[index])
+        new_coloc_map = np.array(new_coloc_map)
+        new_map = new_coloc_map.mean(axis=0)
+        parsed_caption.append(new_word_token)
+        parsed_coloc_maps.append(new_map)
+
+    return parsed_caption, parsed_coloc_maps
+
+
 def flickr_element_processor(element, parse_mode, data):
     """
     Clips co_loc maps and captions for FLICKR data points
@@ -127,6 +156,7 @@ def flickr_element_processor(element, parse_mode, data):
     Processed image has been converted to np images ready to be plotted
     """
     caption_data = data[element['caption']]
+    index_list = caption_data['caption_index']
     coloc_map = element['coloc_map']
     if parse_mode == 'phrase':
         caption = caption_data['parsed_caption']
@@ -135,6 +165,9 @@ def flickr_element_processor(element, parse_mode, data):
     actual_length = len(caption)
     del coloc_map[(actual_length+1):]
     del coloc_map[0]
+
+    if parse_mode != "phrase":
+        caption, coloc_map = caption_parser(caption, coloc_map, index_list)
 
     coloc_map = coloc_map_processor(coloc_map)
 
@@ -167,5 +200,77 @@ def coco_element_processor(element):
     element['image'] = tensor2img(element['image'])
 
     return element
+
+
+def list2string(query_list):
+    """
+    Convert a list of strings to a space separated string
+    :param query_list: a list of strings
+    :return final_string: a string where each token is joined by " "
+    """
+    assert type(query_list) is list, "Lists accepted only"
+    separator = ' '
+    final_string = separator.join(query_list)
+    return final_string
+
+
+def phrase_detokenizer(caption):
+    """
+    Converts a list of tokenized phrases into a list of phrases
+    :param caption: List of tokenized phrases. Each phrase is a list of tokens
+    :return new_caption: List of strings. Each string is a phrase
+    """
+    new_caption = list()
+    for phrase in caption:
+        new_caption.append(list2string(phrase))
+    return new_caption
+
+
+
+def mask_viz(mask_list, caption, bw_img, save_flag=False, save_name=''):
+
+    fig = plt.figure(figsize=(100,30), facecolor="white")
+    columns = len(mask_list) + 1
+    rows = 1
+    for id in range(len(mask_list)):
+        cap_phrase = caption[id]
+        mask = cv2.resize(mask_list[id], dsize=(224,224))
+        fig.add_subplot(rows, columns, id + 1)
+        plt.imshow(bw_img)
+        plt.imshow(mask, cmap='jet', alpha=0.5)
+        plt.title(cap_phrase, fontdict={'fontsize': 10})
+        plt.axis('off')
+    plt.show()
+
+    if save_flag:
+        fig.savefig(save_name)
+
+
+
+def seg_viz(mask_list, caption, color_img, thresh, save_flag=False, save_name=''):
+
+    fig = plt.figure(figsize=(100,30), facecolor="white")
+    columns = len(mask_list) + 1
+    rows = 1
+
+    for id in range(len(mask_list)) + 1:
+        cap_phrase = caption[id]
+        mask = cv2.resize(mask_list[id], dsize=(224, 224))
+        mask2 = np.where((mask < thresh * np.mean(mask)), 0, 1).astype('uint8')
+
+        fig.add_subplot(rows, columns, id + 1)
+        img = color_img * mask2[:, :, np.newaxis]
+
+        plt.imshow(mask, cmap='jet', alpha=0.5)
+        plt.title(cap_phrase, fontdict={'fontsize': 10})
+        plt.axis('off')
+
+    plt.show()
+
+    if save_flag:
+        fig.savefig(save_name)
+
+
+
 
 
