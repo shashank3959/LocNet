3ef95d77fc646ce4a9b1c89d0509ee88ed8a9d13
deepandas11 <deepandas11@gmail.com>
2019-06-05 15:15:36 -0500

    Modifying script to evaluate quantitative score for localization using the various trained models. Models have to be saved in the saved models folder. Based on the name of the models, one has to update the dic_models path dictionary. parse mode refers to what kind of representation to use when parsing phrase: average localization maps or average glove embeddings.
    Signed-off-by: deepandas11 <deepandas11@gmail.com>

--- eval_score.py
+++ eval_score.py
@@ -4,20 +4,22 @@ from visualizations import *
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
 
-parser.add_argument('--model_path', default=0, type=int)
-parser.add_argument('--parse_mode', defaut='phrase', type=str)
+parser.add_argument('--model_path', default=0, type=str)
+parser.add_argument('--parse_mode', default='phrase', type=str)
 
-dict_models = {none_model_path:'',
-			   model_path_30_epochs:'saved_models/flickr_30epochs.tar',
-			   model_path_100_epochs:'saved_models/flickr_100epochs.tar'}
+dict_models = {'none':'',
+        'f_30':'saved_models/flickr_30epochs.tar',
+        'f_100':'saved_models/flickr_100epochs.tar',
+        'c_70': 'saved_models/coco_70epochs.tar',
+        'mix': 'saved_models/mix_140epochs.tar'}
 
-model_paths = list(dict_models.values())
+
 
 args = parser.parse_args()
 
-model_path = model_paths[args.model_path]
+model_path = dict_models[args.model_path]
 
-flickr_processor = FlickrViz(batch_size = 1, parse_mode=args.parse_mode, model_path=, eval_mode=True)
+flickr_processor = FlickrViz(batch_size = 1, parse_mode=args.parse_mode, model_path=model_path, eval_mode=True)
 length_dataset = len(flickr_processor.dataset)
 
 score, score_list = flickr_processor.loc_eval(length_dataset)
